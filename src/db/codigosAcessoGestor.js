f3ed2c4264b394f9787970f6fa911082f780730e
Lucas Lousada <lucas_lousada@outlook.com>
2023-06-25 16:32:28 -0300

    página cadastrar usuário finalizada

---
+++ src/db/codigosAcessoGestor.js
@@ -0,0 +1,12 @@
+export const codigosAcessoGestor = [
+  'KSLX39K',
+  'L29CZI8',
+  '9LZOSI2',
+  '0SS09ZX',
+  '12KXLZK',
+  '0XXIWKC',
+  'MVKWISD',
+  'IWKDIGJ',
+  '82IFKDJ',
+  '92847FJ',
+];
---
+++ src/pages/cadastro-usuario/script.js
@@ -0,0 +1,57 @@
+import { codigosAcessoGestor } from '../../db/codigosAcessoGestor';
+
+const form = document.getElementById('formulario-cadastro-usuario');
+form.addEventListener('submit', cadastrarUsuario);
+
+function cadastrarUsuario(e) {
+  e.preventDefault();
+
+  const nome = document.getElementById('nome').value;
+  const sobrenome = document.getElementById('sobrenome').value;
+  const cidade = document.getElementById('cidade').value;
+  const bairro = document.getElementById('bairro').value;
+  const profissao = document.getElementById('profissao').value;
+  const codigoGestor = document.getElementById('codigo-gestor').value;
+  const email = document.getElementById('email').value;
+  const senha = document.getElementById('senha').value;
+
+  const usuarios = JSON.parse(localStorage.getItem('@nomeapp:usuarios'));
+
+  const usuarioExiste = usuarios?.find(usuario => email === usuario.email);
+
+  if (usuarioExiste) {
+    alert('Este usuário já existe.');
+    return;
+  }
+
+  const novoUsuario = {
+    id: usuarios?.length || 0,
+    nome,
+    sobrenome,
+    cidade,
+    bairro,
+    profissao,
+    email,
+    senha,
+    tipo_usuario: null,
+    reclamacoes_salvas: [],
+  };
+
+  if (codigoGestor) {
+    if (codigosAcessoGestor.includes(codigoGestor))
+      novoUsuario.tipo_usuario = 'gestor';
+    else {
+      alert('Código de acesso inválido.');
+      return;
+    }
+  } else {
+    novoUsuario.tipo_usuario = 'comum';
+  }
+
+  localStorage.setItem(
+    '@nomeapp:usuarios',
+    JSON.stringify(usuarios ? [...usuarios, novoUsuario] : [novoUsuario])
+  );
+
+  window.location.href = '../login/index.html';
+}
