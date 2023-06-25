0f3888108661bb8d4fb52f160bd99fed9ecf5ada
Lucas Lousada <lucas_lousada@outlook.com>
2023-06-25 16:33:03 -0300

    página recuperar senha finalizada

---
+++ src/pages/recuperar-senha/script.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('formulario-recuperar-senha');
+form.addEventListener('submit', recuperarSenha);
+
+function recuperarSenha(e) {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const novaSenha = document.getElementById('nova-senha').value;
+  const repetirNovaSenha = document.getElementById('repetir-nova-senha').value;
+
+  const usuarios = JSON.parse(localStorage.getItem('@nomeapp:usuarios'));
+
+  const usuarioExiste = usuarios?.find(usuario => email === usuario.email);
+
+  if (!usuarioExiste) {
+    alert('Este usuário não existe.');
+    return;
+  }
+
+  if (novaSenha !== repetirNovaSenha) {
+    alert('As senhas informadas não são iguais.');
+    return;
+  }
+
+  const usuariosEditados = usuarios.map(usuario =>
+    email === usuario.email ? { ...usuario, senha: novaSenha } : usuario
+  );
+
+  localStorage.setItem('@nomeapp:usuarios', JSON.stringify(usuariosEditados));
+
+  alert('Senha alterada com sucesso');
+
+  window.location.href = '../login/index.html';
+}
