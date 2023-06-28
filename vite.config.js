33b7e733ee55071b6c36be386b7cd99d07da4a6d
Lucas Lousada <lucas_lousada@outlook.com>
2023-06-28 18:54:21 -0300

    atualizar diretório raiz do projeto, vitejs

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,27 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-const root = resolve(__dirname, 'src/pages');
+const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
+  server: {
+    open: 'pages/login/index.html',
+  },
   root,
   build: {
     outDir,
     rollupOptions: {
       input: {
-        main: resolve(root, 'index.html'),
-        login: resolve(root, 'login/index.html'),
-        cadastro_usuario: resolve(root, 'cadastro-usuario/index.html'),
-        criar_reclamacao: resolve(root, 'criar-reclamacao/index.html'),
-        feed_de_reclamacoes: resolve(root, 'feed-de-reclamacoes/index.html'),
-        recuperar_senha: resolve(root, 'recuperar-senha/index.html'),
+        main: resolve(root, 'pages/index.html'),
+        login: resolve(root, 'pages/login/index.html'),
+        cadastro_usuario: resolve(root, 'pages/cadastro-usuario/index.html'),
+        criar_reclamacao: resolve(root, 'pages/criar-reclamacao/index.html'),
+        feed_de_reclamacoes: resolve(
+          root,
+          'pages/feed-de-reclamacoes/index.html'
+        ),
+        recuperar_senha: resolve(root, 'pages/recuperar-senha/index.html'),
       },
     },
   },
