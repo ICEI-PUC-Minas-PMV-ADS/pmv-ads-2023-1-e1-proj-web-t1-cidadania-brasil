a4abfb2f75294095b3bc105e5bb3e07560acd1e7
Lucas Lousada <lucas_lousada@outlook.com>
2023-06-28 18:08:31 -0300

    alterações para a versão de produção vitejs

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
+const root = resolve(__dirname, 'src/pages');
+const outDir = resolve(__dirname, 'dist');
+
 export default defineConfig({
-  root: './src',
+  root,
   build: {
-    outDir: resolve(__dirname, 'dist'),
+    outDir,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        login: resolve(__dirname, 'src/pages/login/index.html'),
-        cadastro_usuario: resolve(__dirname, 'src/pages/cadastro-usuario/index.html'),
-        criar_reclamacao: resolve(__dirname, 'src/pages/criar-reclamacao/index.html'),
-        feed_de_reclamacoes: resolve(__dirname, 'src/pages/feed-de-reclamacoes/index.html'),
-        recuperar_senha: resolve(__dirname, 'src/pages/recuperar-senha/index.html'),
+        main: resolve(root, 'index.html'),
+        login: resolve(root, 'login/index.html'),
+        cadastro_usuario: resolve(root, 'cadastro-usuario/index.html'),
+        criar_reclamacao: resolve(root, 'criar-reclamacao/index.html'),
+        feed_de_reclamacoes: resolve(root, 'feed-de-reclamacoes/index.html'),
+        recuperar_senha: resolve(root, 'recuperar-senha/index.html'),
       },
     },
   },
-})
+});
