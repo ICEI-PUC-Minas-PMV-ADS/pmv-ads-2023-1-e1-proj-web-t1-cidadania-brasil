62d02da93d58032fb3a8b220302ddb6b422422e1
Lucas Lousada <lucas_lousada@outlook.com>
2023-06-28 14:14:13 -0300

    configurações de produção vitejs

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-export default {
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   root: './src',
   build: {
-    outDir: '../dist',
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        login: resolve(__dirname, 'src/pages/login/index.html'),
+        cadastro_usuario: resolve(__dirname, 'src/pages/cadastro-usuario/index.html'),
+        criar_reclamacao: resolve(__dirname, 'src/pages/criar-reclamacao/index.html'),
+        feed_de_reclamacoes: resolve(__dirname, 'src/pages/feed-de-reclamacoes/index.html'),
+        recuperar_senha: resolve(__dirname, 'src/pages/recuperar-senha/index.html'),
+      },
+    },
   },
-};
+})
